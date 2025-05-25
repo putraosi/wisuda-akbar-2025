7311613d3000d6dea8b41c47005893306a1589d8
Osi <osi@tebengan.id>
2025-05-25 13:50:07 +0700

    fix linking wa

--- src/helpers/linking.ts
+++ src/helpers/linking.ts
@@ -8,7 +8,7 @@ export const openWhatsApp = (phone: string) => {
   if (phone.slice(0, 1) === "0") newPhone = phone.slice(1);
   else if (phone.slice(0, 3) === "+62") newPhone = phone.slice(3);
 
-  const url = `https://wa.me/62${phone}`;
+  const url = `https://wa.me/62${newPhone}`;
 
   window.open(url, "_blank");
 };
